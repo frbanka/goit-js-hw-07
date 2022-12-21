d3fe27a0456dcb5d5646fea6cad1b956d738b6df
Jacek Wójciak <fr.banka@gmail.com>
2022-12-21 21:26:48 +0100

    new code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryIt = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", galleryIt);
+gallery.addEventListener("click", openImage);
 
-console.log(galleryItems);
+function openImage(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800" height="600">`,
+    {
+      open: () => window.addEventListener("keydown", escExit),
+      close: () => window.removeEventListener("keydown", escExit),
+    }
+  );
+  instance.show();
+
+  function escExit(e) {
+    if (e.code === "Escape") {
+      modal.close();
+    }
+  }
+}

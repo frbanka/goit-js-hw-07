645ec27861aad8ed161e02e9d15f967d0613a72d
Jacek Wójciak <fr.banka@gmail.com>
2022-12-28 17:42:24 +0100

    work2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryIt = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", galleryIt);
 
-console.log(galleryItems);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
